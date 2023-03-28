906348367e3ded4d122e2b78155b61ef5c3600d5
Hanna Madsen <hannamymadsen89@gmail.com>
2023-03-28 08:54:54 +0200

    allt

--- skriptosv/Cloud_Init/DeployShit.sh
+++ skriptosv/Cloud_Init/DeployShit.sh
@@ -1,9 +1,9 @@
 #!bin/bash
 
 PublicKey=$(cat ~/.ssh/id_rsa.pub)
-templatePath=~/Documents/CLO22Dev/InlämningARM/UppgiftTwo/skriptosv/armtemplate.json
-RG=blobbenbob
-deployName=BastionDeployScript
+templatePath=c:/Users/89hanmad/Desktop/uppgifttvålättare/UppgiftTv-/skriptosv/downloadtemplate.json
+RG=hannarg2
+deployName=DeployScript
 
-az deployment group create  --name $deployName --resource-group $RG --template-file $templatePath --parameters AdminPublicKey="$PublicKey" NginxInit=@./Cloud_Init/Cloud_Nginx.yaml DotnetInit=@./Cloud_Init/Cloud_dotnet7.yaml
+az deployment group create  --name $deployName --resource-group $RG --template-file $templatePath  
 sleep 5s
